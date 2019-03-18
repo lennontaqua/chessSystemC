d9f63a6485343c135dc751a6b3cbcaa184a48b71
lennontaqua <lennontaqua@hotmail.com>
2019-03-17 22:17:42 -0300

    Created Class Position

---
+++ ChessSystem/Tabuleiro/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessSystem.Tabuleiro
+{
+    class Position
+    {
+        public int row { set; get; }
+        public int column { set; get; }
+
+        public Position(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return row + ", " + column;
+        }
+    }
+}
