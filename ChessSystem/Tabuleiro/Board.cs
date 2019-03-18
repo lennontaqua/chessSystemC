91726033ff534645b185c05c0bcb2990319cd796
lennontaqua <lennontaqua@hotmail.com>
2019-03-17 22:41:10 -0300

    Created Class Piece and Board

---
+++ ChessSystem/Tabuleiro/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessSystem.Tabuleiro
+{
+    class Board
+    {
+        public int rows { get; set; }
+        public int columns { get; set; }
+        private Piece[,] piece;
+
+        public Board(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            piece = new Piece[rows, columns];
+        }
+    }
+}
---
+++ ChessSystem/Tabuleiro/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessSystem.Tabuleiro
+{
+    class Piece
+    {
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+        public int qtyMove { get; protected set; }
+        public Board board { get; protected set; }
+
+        public Piece(Position position, Board board, Color color)
+        {
+            this.position = position;
+            this.board = board;
+            this.color = color;
+            this.qtyMove = 0;
+        }
+    }
+}
